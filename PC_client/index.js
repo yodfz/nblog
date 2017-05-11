cd10e34afb8918725ab802865055436fca9cd4a4
yodfz <yodfz@qq.com>
2017-05-11 09:30:27 +0800

    update

--- PC_client/index.js
+++ PC_client/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
 import styles from './index.less';
+
+// let store = createStore(todoApp)
 
 import ManageMain from './views/manage/Main';
 // import HelloWorld from './components/HelloWorld';
