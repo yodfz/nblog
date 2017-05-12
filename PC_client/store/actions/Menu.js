a5b81d66bc12683e024f87e6954904ac8d74caa7
yodfz <yodfz@qq.com>
2017-05-12 09:52:10 +0800

    add menu

--- PC_client/store/actions/Menu.js
+++ PC_client/store/actions/Menu.js
@@ -1,3 +1,8 @@
+/**
+ * 用于将修改指令进行封装，否则需要自己进行指令的封装
+ * old: dispatch({type:SELECT_MENU,payLoad:{index}})
+ * now: dispatch(selectMenu(index));
+ */
 import actionsType, {SELECT_MENU} from '../actionsType';
 export function selectMenu (index) {
     console.log('index',index);
--- PC_client/store/actionsType.js
+++ PC_client/store/actionsType.js
@@ -1,3 +1,7 @@
+/**
+ * actionsType 表示系统中所有的修改数据指令名称
+ * @type {string}
+ */
 const SELECT_MENU = 'SELECT_MENU';
 export {
     SELECT_MENU
