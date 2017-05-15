aa6de0d9dc77c7027027aa39db7bc9ee023d01f4
yodfz <yodfz@qq.com>
2017-05-15 23:17:02 +0800

    add article list

---
+++ PC_client/components/Article/List.js
@@ -0,0 +1,22 @@
+import React, {Component,PropTypes} from 'react';
+import styles from './Article.less';
+
+export default class List  extends Component {
+    static defaultProps = {
+    };
+    static propTypes = {
+    };
+    constructor () {
+        super()
+    };
+    componentWillMount () {}
+    componentDidMount  () {}
+    shouldComponentUpdate () {}
+    componentWillUpdate () {}
+    componentDidUpdate () {}
+    componentWillUnmount () {}
+
+    render () {
+        return (<div></div>);
+    }
+}
