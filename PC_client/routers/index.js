63fac53e1f9edc29338408d09e5f31bd3aec160a
yodfz <yodfz@qq.com>
2017-05-23 09:44:24 +0800

    add routers

--- PC_client/routers/index.js
+++ PC_client/routers/index.js
@@ -1,7 +1,12 @@
-import {} from 'react-router';
+import {IndexRoute,Router, Route} from 'react-router';
+
+// UI COMPONENTS
+import Main from '../views/manage/Main'
 
 export default ({history})=>
     <Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
-        <Route>
+        <Route path="/manage" component={Main}>
+
+            {/*<IndexRoute component={Dashboard} />*/}
         </Route>
     </Router>;
--- PC_client/views/manage/Main.js
+++ PC_client/views/manage/Main.js
@@ -1,8 +1,8 @@
 import React, {Component, PropTyps} from 'react';
 import LeftMenu from './LeftMenu';
-import MiddleArticleList from './MiddleArticleList';
-import RightDetail from './RightDetail';
-import ArticleDetail from './Article/Detail';
+// import MiddleArticleList from './MiddleArticleList';
+// import RightDetail from './RightDetail';
+// import ArticleDetail from './Article/Detail';
 
 export default class Main extends Component {
     render () {
@@ -10,8 +10,8 @@ export default class Main extends Component {
             <div className="vh100 main">
                 <LeftMenu/>
                 {this.props.children}
-                <MiddleArticleList/>
-                <RightDetail></RightDetail>
+                {/*<MiddleArticleList/>*/}
+                {/*<RightDetail></RightDetail>*/}
             </div>
         );
     }
