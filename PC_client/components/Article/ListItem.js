3bd7dc122f4e14525b5c7f02aca670f0b210afad
yodfz <yodfz@qq.com>
2017-06-02 15:29:46 +0800

    update article list

--- PC_client/components/Article/ListItem.js
+++ PC_client/components/Article/ListItem.js
@@ -8,6 +8,7 @@ export default class ListItem extends Component {
     constructor (props) {
         super(props);
         this.model = props.data;
+        this.select = props.select;
     };
 
     componentWillMount () {
@@ -30,11 +31,18 @@ export default class ListItem extends Component {
     }
 
     render () {
-        return (<div className={styles.ListItem + ' ' + (this.props.className || '')}>
+        const formatDate = (str) => {
+            let $date = new Date(str);
+            return $date.getFullYear() + '-' + ($date.getMonth() + 1) + '-' + $date.getDate();
+        };
+        return (<div onClick={this.select.bind(this, this.model.idx)}
+                     className={styles.ListItem + ' ' + (this.props.className || '')}>
             <h3>{this.model.title}</h3>
             <p className="description">{this.model.description}</p>
-            <span className="description">{this.model.tag}</span>
-            <span className="description par">{this.model.createdAt}</span>
+            <span className="description">{this.model.tag.split(' ').map(p=> {
+                return '#' + p + ' ';
+            })}</span>
+            <span className="description par">{formatDate(this.model.createdAt)}</span>
         </div>);
     }
 }
--- PC_client/store/reducer/Article.js
+++ PC_client/store/reducer/Article.js
@@ -1,21 +1,26 @@
 import {SELECT_ARTICLE, UPDATE_ARTICLE_STATE, APPEND_ARTICLE_LIST} from '../actionsType';
 
 const defaultState = {
+    // 当前状态
     status: '',
+    // 选中的文章 0 为创建的
     select: {},
+    // 选中文章的idx
+    selectIdx: -1,
+    // 文章列表数据
     data: [],
     detail: {}
 };
 
 
 export default (state = defaultState, action)=> {
-    console.log('article action', action);
     switch (action.type) {
         case SELECT_ARTICLE: {
             if (action.payLoad.id == -1) {
                 return Object.assign({}, state, {select: {}});
             }
-            return Object.assign({}, state, {select: {id: action.payLoad.index}});
+            let selectArticle = state.data.find(p=>p.idx == action.payLoad.id);
+            return Object.assign({}, state, {select: selectArticle, selectIdx: selectArticle.idx});
         }
             break;
         case UPDATE_ARTICLE_STATE: {
