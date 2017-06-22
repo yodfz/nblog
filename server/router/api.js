60bf6a3a56be0072bcbe911afe52d9533d1a35d3
yodfz <yodfz@qq.com>
2017-06-22 17:04:11 +0800

    完成文章界面

--- server/router/api.js
+++ server/router/api.js
@@ -24,6 +24,7 @@ router.post(apiPre + 'article/save', async (ctx)=> {
         } else {
             ctx.body = createMessage($data, {});
         }
+        return;
     }
     ctx.body = '';
     ctx.status = 401;
--- server/router/render.js
+++ server/router/render.js
@@ -8,29 +8,74 @@ var path = require("path");
 var template = require('art-template');
 var $config = require('../config');
 var model = require('../model');
+var marked = require('marked');
+template.defaults.minimize = true;
 router.get('/login', ctx=> {
     ctx.body = template(path.resolve(__dirname, '../template/login.html'), {
-        user:{'name': 'Hello World'}
+        user: {'name': 'Hello World'}
     });
 });
 
-router.get('/',async (ctx)=> {
+router.get('/', async (ctx)=> {
     let data = await model.article.findAndCountAll({
         offset: 0,
         limit: $config.pageSize,
         order: [['idx', 'DESC']]
     });
+    let photoData = await model.photo.findAndCountAll({
+        offset: 0,
+        limit: 12,
+        order: [['idx', 'DESC']]
+    });
     template.defaults.imports.Date = Date;
     ctx.body = template(path.resolve(__dirname, '../template/index.html'), {
-        config:$config.web,
-        article:data
+        config: $config.web,
+        article: data,
+        photo: photoData
     });
 });
 
 
 router.get('/manage*', ctx=> {
-    ctx.body = template(path.resolve(__dirname, '../template/manage/index.html'), {
-    });
+    if (ctx.session.isLogin) {
+        ctx.body = template(path.resolve(__dirname, '../template/manage/index.html'), {});
+    } else {
+        // 重定向到登陆页
+        ctx.redirect('/login');
+    }
+});
+
+router.get('/detail/:id/:title', async ctx=> {
+    let data = await model.article.findOne({where: {idx: ctx.params.id}});
+    if (data) {
+        data.viewCount += 1;
+        $data = await model.article.update({viewCount: data.viewCount}, {
+            where: {idx: data.idx}
+        });
+        template.defaults.imports.Date = Date;
+        data.content = marked(data.content);
+        ctx.body = template(path.resolve(__dirname, '../template/article_detail.html'), {
+            config: $config.web,
+            data: data
+        });
+    } else {
+        ctx.body = template(path.resolve(__dirname, '../template/404.html'), {
+            config: $config.web
+        });
+    }
+
+});
+
+router.get('/article/:pageIndex', ctx=> {
+    ctx.body = 'test';
+});
+
+router.get('/photo/:pageIndex', ctx=> {
+    ctx.body = 'test';
+});
+
+router.get('/photoDetail/:idx', ctx=> {
+    ctx.body = 'test';
 });
 
 
