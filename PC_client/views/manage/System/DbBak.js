a0e50a9ae8a0b5d41e483fe4bf67484b4a6c7f4f
yodfz <yodfz@qq.com>
2017-06-29 16:51:35 +0800

    add dbbank

---
+++ PC_client/views/manage/System/DbBak.js
@@ -0,0 +1,21 @@
+import React, {Component,PropTypes} from 'react';
+
+export default class DbBak extends Component {
+    static defaultProps = {
+    };
+    static propTypes = {
+    };
+    constructor () {
+        super()
+    };
+    componentWillMount () {}
+    componentDidMount  () {}
+    shouldComponentUpdate () {return true;}
+    componentWillUpdate () {}
+    componentDidUpdate () {}
+    componentWillUnmount () {}
+
+    render () {
+        return (<div></div>);
+    }
+}
