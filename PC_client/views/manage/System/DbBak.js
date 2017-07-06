298a58976f13ea53771aeca4489f15e0f1012ce4
yodfz <yodfz@qq.com>
2017-07-06 17:14:53 +0800

    add dbbank

--- PC_client/views/manage/System/DbBak.js
+++ PC_client/views/manage/System/DbBak.js
@@ -1,4 +1,5 @@
 import React, {Component,PropTypes} from 'react';
+import styles from './index.less';
 
 export default class DbBak extends Component {
     static defaultProps = {
@@ -16,6 +17,24 @@ export default class DbBak extends Component {
     componentWillUnmount () {}
 
     render () {
-        return (<div></div>);
+        return (<div className={[styles.index,styles.DbBak]}>
+            <div className="nav">
+                <div className="menu select">修改密码</div>
+                <div className="menu">数据库备份</div>
+            </div>
+            <div className="DbBakIndex">
+                <p>
+                    最后备份时间:  (mb)
+                </p>
+                <p>
+
+                </p>
+                <p>
+                    <button className="btn submit">
+                        备份数据库
+                    </button>
+                </p>
+            </div>
+        </div>);
     }
 }
