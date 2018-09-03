e7d887b11dc80bfd093d957d9a15ba48db6a5a31
zhaoyifeng <yodfz@qq.com>
2018-09-03 23:49:28 +0800

    style(全局样式): 部分填充

---
+++ scp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin
+scp -r ./package.json root@yodfz.com:/wwwroot/www.yodfz.com
+scp -r ./server root@yodfz.com:/wwwroot/www.yodfz.com
---
+++ scp_pub_template.sh
@@ -0,0 +1,3 @@
+#!/usr/bin
+scp -r ./server/template root@yodfz.com:/wwwroot/www.yodfz.com/server
+scp -r ./server/public/styles root@yodfz.com:/wwwroot/www.yodfz.com/server/public
