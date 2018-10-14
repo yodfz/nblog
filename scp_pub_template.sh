26f5e24165c9f3e647f80648761eea464a2723a1
zhaoyifeng <yodfz@qq.com>
2018-10-14 13:30:07 +0800

    feat(文章详情页): 增加mk文章导航列表；增加图片简单的点击放大

--- scp_pub_template.sh
+++ scp_pub_template.sh
@@ -1,4 +1,5 @@
 #!/usr/bin
-echo scp -r ./server/template root@yodfz.com:/wwwroot/www.yodfz.com/server
-echo scp -r ./server/public/styles root@yodfz.com:/wwwroot/www.yodfz.com/server/public
+scp -r ./server/template root@yodfz.com:/wwwroot/www.yodfz.com/server
+scp -r ./server/public/styles root@yodfz.com:/wwwroot/www.yodfz.com/server/public
 scp -r ./server/public/static/images root@yodfz.com:/wwwroot/www.yodfz.com/server/public/static
+scp -r ./server/public/js root@yodfz.com:/wwwroot/www.yodfz.com/server/public
