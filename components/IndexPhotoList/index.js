2fca21e3c0d3c9024fdc89a4f1460d68ad4d70f0
yodfz@server <yodfz@qq.com>
2019-08-30 18:36:23 +0800

    update

--- components/IndexPhotoList/index.js
+++ components/IndexPhotoList/index.js
@@ -3,8 +3,11 @@ import './index.less'
 export default class Index extends react.PureComponent {
     render () {
         const data = this.props.data.data
-        return <div>
+        return <>
+        <p className="photoTitle">MY PHOTO</p>
+        <div>
             {data.map(item=><div className="thumbImg"><img src={`https://www.yodfz.com${item.url.replace('.','_thumb.')}`} /></div>)}
         </div>
+        </>
     }
 }
